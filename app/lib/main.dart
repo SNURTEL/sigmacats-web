048ff596996d1a7cd5a8e1452d1a65b10e57a273
towienko <01169218@pw.edu.pl>
2023-12-11 21:02:33 +0100

    Basic race list view

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,158 +1,215 @@
+import 'dart:convert';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:sizer/sizer.dart';
+
+import 'models/Race.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Coordinator',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Coordinator panel'),
+    return Sizer(
+        builder: (context, orientation, deviceType) {
+          return MaterialApp(
+            title: 'Coordinator',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: RaceList(),
+          );();
+        }
     );
   }
 }
 
-Future<String> fetchRaces() async {
-  final http.Response response = await http
-      .get(Uri.http('localhost:8000', 'api/coordinator/race'), headers: {
-    "Access-Control-Allow-Origin": "*",
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  });
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    // return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-    return response.body;
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception(
-        'Failed to fetch races with ${response.statusCode}: ${response.body}');
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class RaceList extends StatefulWidget {
+  const RaceList({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _RaceListState createState() => _RaceListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  late Future<String> futureRacesJsonString = Future(() => "Not loaded");
+class _RaceListState extends State<RaceList> {
+  late Future<List<RaceListEntry>> futureRaces;
 
-  // @override
-  // void didChangeDependencies() {
-  //   super.didChangeDependencies();
-  //   futureRacesJsonString = fetchRaces();
-  // }
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    futureRaces = fetchRaceList();
+  }
+
+  Future<List<RaceListEntry>> fetchRaceList() async {
+    final response =
+        await http.get(Uri.parse('http://localhost/api/coordinator/race/'));
+
+    if (response.statusCode == 200) {
+      // If the server returns a 200 OK response, parse the races from the response
+      final List<dynamic> races = json.decode(utf8.decode(response.bodyBytes));
+      return races.map((race) => RaceListEntry.fromMap(race)).toList();
+    } else {
+      // If the server did not return a 200 OK response, throw an exception.
+      throw Exception('Failed to load races');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme
-            .of(context)
-            .colorScheme
-            .inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Hello, <username>!'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Row(
+        children: [
+          NavigationRail(
+            selectedIndex: _selectedIndex,
+            onDestinationSelected: (int index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            labelType: NavigationRailLabelType.all,
+            destinations: [
+              NavigationRailDestination(
+                icon: Icon(Icons.home),
+                label: Text('Home'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.search),
+                label: Text('Search'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.person),
+                label: Text('Profile'),
+              ),
+            ],
+          ),
+          VerticalDivider(thickness: 1, width: 1),
+          Expanded(
+            child: Center(
+              child: _getPage(_selectedIndex, context),
             ),
-            ElevatedButton(
-              style: const ButtonStyle(),
-              onPressed: () {
-                setState(() {
-                  futureRacesJsonString = fetchRaces();
-                });
-              },
-              child: const Text('Press here'),
-            ),
-            FutureBuilder<String>(
-              future: futureRacesJsonString,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Text(snapshot.data!);
-                } else if (snapshot.hasError) {
-                  return Text('${snapshot.error}');
-                }
+          ),
+        ],
+      ),
+    );
+  }
 
-                // By default, show a loading spinner.
-                return const CircularProgressIndicator();
-              },
+  Widget? _getPage(int index, BuildContext context) {
+    switch (index) {
+      case 0:
+        return _buildPage(context);
+      case 1:
+        return _buildPage(context);
+      case 2:
+        return _buildPage(context);
+    }
+    return null;
+  }
+
+  Widget _buildPage(BuildContext context) {
+    return FutureBuilder(
+      future: futureRaces,
+      builder: (context, snapshot) {
+        late Widget content;
+        if (snapshot.hasData) {
+          content = Center(
+            // padding: EdgeInsets.symmetric(vertical: 5.h, horizontal: 3.h),
+            child: Scrollbar(
+              child: SizedBox(
+                width: max(min(40.h, 300), 600),
+                child: ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    return
+                      Column(
+                        children: [
+                          Card(
+                            margin: const EdgeInsets.all(5.0),
+                            child: Column(
+                              children: [
+                                Container(
+                                  height: 160.0,
+                                  // Fixed height
+                                  width: double.infinity,
+                                  // Fill the available width
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(16.0),
+                                      topRight: Radius.circular(16.0),
+                                    ),
+                                    child: Image.asset(
+                                      'res/sample_image.png',
+                                      fit: BoxFit
+                                          .fitWidth, // Ensure the image fills the container
+                                    ),
+                                  ),
+                                ),
+                                ListTile(
+                                  contentPadding: const EdgeInsets.all(10.0),
+                                  title: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        snapshot.data![index].name,
+                                        style: TextStyle(
+                                          fontSize: 20.0,
+                                          // Adjust the font size as needed
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      SizedBox(height: 5.0),
+                                      Text(
+                                        snapshot.data![index].meetup_timestamp
+                                            .toString(),
+                                        style: TextStyle(
+                                          fontSize: 16.0,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 8.0),
+                          // Add space between list elements
+                        ],
+                      );
+                  },
+                ),
+
+              )
             ),
-          ],
+          );
+        } else if (snapshot.hasError) {
+          content = Text('${snapshot.error}');
+        } else {
+          // By default, show a loading spinner.
+          content = const CircularProgressIndicator();
+        }
+
+        return content;
+      },
+    );
+  }
+
+  Widget _buildCard(String content) {
+    return Card(
+      margin: EdgeInsets.all(8),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Text(
+          content,
+          style: TextStyle(fontSize: 18),
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
---
+++ app/lib/models/Race.dart
@@ -0,0 +1,141 @@
+enum RaceStatus {
+  pending("pending"),
+  in_progress("in_progress"),
+  ended("ended"),
+  cancelled("cancelled");
+
+  const RaceStatus(this.value);
+  final String value;
+
+
+}
+
+class RaceListEntry {
+  final int id;
+  final RaceStatus status;
+  final String name;
+  final String description;
+  final int no_laps;
+  final DateTime? meetup_timestamp;
+  final DateTime start_timestamp;
+  final DateTime end_timestamp;
+  final String event_graphic_file;
+  final int season_id;
+
+//<editor-fold desc="Data Methods">
+  const RaceListEntry({
+    required this.id,
+    required this.status,
+    required this.name,
+    required this.description,
+    required this.no_laps,
+    this.meetup_timestamp,
+    required this.start_timestamp,
+    required this.end_timestamp,
+    required this.event_graphic_file,
+    required this.season_id,
+  });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is RaceListEntry &&
+          runtimeType == other.runtimeType &&
+          id == other.id &&
+          status == other.status &&
+          name == other.name &&
+          description == other.description &&
+          no_laps == other.no_laps &&
+          meetup_timestamp == other.meetup_timestamp &&
+          start_timestamp == other.start_timestamp &&
+          end_timestamp == other.end_timestamp &&
+          event_graphic_file == other.event_graphic_file &&
+          season_id == other.season_id);
+
+  @override
+  int get hashCode =>
+      id.hashCode ^
+      status.hashCode ^
+      name.hashCode ^
+      description.hashCode ^
+      no_laps.hashCode ^
+      meetup_timestamp.hashCode ^
+      start_timestamp.hashCode ^
+      end_timestamp.hashCode ^
+      event_graphic_file.hashCode ^
+      season_id.hashCode;
+
+  @override
+  String toString() {
+    return 'RaceListEntry{' +
+        ' id: $id,' +
+        ' status: $status,' +
+        ' name: $name,' +
+        ' description: $description,' +
+        ' no_laps: $no_laps,' +
+        ' meetup_timestamp: $meetup_timestamp,' +
+        ' start_timestamp: $start_timestamp,' +
+        ' end_timestamp: $end_timestamp,' +
+        ' event_graphic_file: $event_graphic_file,' +
+        ' season_id: $season_id,' +
+        '}';
+  }
+
+  RaceListEntry copyWith({
+    int? id,
+    RaceStatus? status,
+    String? name,
+    String? description,
+    int? no_laps,
+    DateTime? meetup_timestamp,
+    DateTime? start_timestamp,
+    DateTime? end_timestamp,
+    String? event_graphic_file,
+    int? season_id,
+  }) {
+    return RaceListEntry(
+      id: id ?? this.id,
+      status: status ?? this.status,
+      name: name ?? this.name,
+      description: description ?? this.description,
+      no_laps: no_laps ?? this.no_laps,
+      meetup_timestamp: meetup_timestamp ?? this.meetup_timestamp,
+      start_timestamp: start_timestamp ?? this.start_timestamp,
+      end_timestamp: end_timestamp ?? this.end_timestamp,
+      event_graphic_file: event_graphic_file ?? this.event_graphic_file,
+      season_id: season_id ?? this.season_id,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': this.id,
+      'status': this.status.value,
+      'name': this.name,
+      'description': this.description,
+      'no_laps': this.no_laps,
+      'meetup_timestamp': this.meetup_timestamp,
+      'start_timestamp': this.start_timestamp,
+      'end_timestamp': this.end_timestamp,
+      'event_graphic_file': this.event_graphic_file,
+      'season_id': this.season_id,
+    };
+  }
+
+  factory RaceListEntry.fromMap(Map<String, dynamic> map) {
+    return RaceListEntry(
+      id: map['id'] as int,
+      status: RaceStatus.values.byName(map['status']),
+      name: map['name'] as String,
+      description: map['description'] as String,
+      no_laps: map['no_laps'] as int,
+      meetup_timestamp: DateTime.parse(map['meetup_timestamp']),
+      start_timestamp: DateTime.parse(map['start_timestamp']),
+      end_timestamp: DateTime.parse(map['end_timestamp']),
+      event_graphic_file: map['event_graphic_file'] as String,
+      season_id: map['season_id'] as int,
+    );
+  }
+
+//</editor-fold>
+}
